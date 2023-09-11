ebc2e911a9354028c8058e7de9060cd20bdae7cc
Vickyvignesh-01 <vickie7210@gmail.com>
2023-09-11 11:25:40 +0530

    updated codings

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglefunc() {
+    const menu = document.querySelector(".hamburger-links");
+    const icon = document.querySelector(".threelines");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
